a1ae45f557ecebe171f7724e0c296eb16f77b4e2
Eetu <99178022+E3TU@users.noreply.github.com>
2026-01-14 23:40:38 +0200

    Topbar component

---
+++ frontend/src/components/Topbar.svelte
@@ -0,0 +1,16 @@
+<div class="topbar"></div>
+
+<style>
+	.topbar {
+		width: 95%;
+		height: 5rem;
+		margin-top: 1rem;
+		border-radius: 15px;
+		background: linear-gradient(90deg, var(--nord-dark) 0%, var(--nord-light) 100%);
+		box-shadow:
+			inset 2px 2px 5px rgba(0, 0, 0, 0.2),
+			inset -2px -2px 5px rgba(255, 255, 255, 0.055),
+			5px 5px 10px rgba(0, 0, 0, 0.3),
+			-5px -5px 10px rgba(255, 255, 255, 0.041);
+	}
+</style>
