aa10c5ac486db289574a5ec99bd2be7dd4422ee2
Eetu <99178022+E3TU@users.noreply.github.com>
2026-01-25 22:22:23 +0200

    Created maincontainer component

---
+++ frontend/src/components/MainContainer.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    let { children } = $props();
+</script>
+
+<div class="main-container">
+{@render children?.()}
+</div>
+
+<style>
+	.main-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		height: 100vh;
+		width: 100%;
+		background-color: var(--bg-black);
+		background-image: url('ffflux.svg');
+		background-repeat: no-repeat;
+		background-position: center;
+		background-size: contain;
+	}
+</style>
