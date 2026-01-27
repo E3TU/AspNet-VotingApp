5fe74915cb0091acc1d4cc3a01461086016a06c9
Eetu <99178022+E3TU@users.noreply.github.com>
2026-01-27 20:22:39 +0200

    Updated box shadow and width

--- frontend/src/components/Topbar.svelte
+++ frontend/src/components/Topbar.svelte
@@ -2,15 +2,12 @@
 
 <style>
 	.topbar {
-		width: 98%;
+		width: 80%;
 		height: 5rem;
 		margin-top: 1rem;
 		border-radius: 15px;
 		background: linear-gradient(90deg, var(--nord-dark) 0%, var(--nord-light) 100%);
-		box-shadow:
-			inset 2px 2px 5px rgba(0, 0, 0, 0.2),
-			inset -2px -2px 5px rgba(255, 255, 255, 0.055),
-			5px 5px 10px rgba(0, 0, 0, 0.3),
-			-5px -5px 10px rgba(255, 255, 255, 0.041);
+		box-shadow:  5px 5px 10px #0b0c10,
+             -5px -5px 10px #2b2e3e;
 	}
 </style>
