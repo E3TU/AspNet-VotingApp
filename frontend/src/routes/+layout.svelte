ded9129be58318b236a18c7b6aa4a9b04cdabeff
Eetu <99178022+E3TU@users.noreply.github.com>
2026-02-08 22:11:58 +0200

    Fade animation for page transitions

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,13 +1,32 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
 	import '@fontsource-variable/geologica';
-	import "../app.css";
+	import '../app.css';
 
-	let { children } = $props();
+	import { fade } from 'svelte/transition';
+
+	let { data, children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+<div class="wrapper">
+	{#key data.pathname}
+		<div class="page" in:fade={{ duration: 400}} out:fade={{ duration: 300 }}>
+			{@render children()}
+		</div>
+	{/key}
+</div>
 
-{@render children()}
+<style>
+	.wrapper{
+		position: relative;
+		min-height: 100vh;
+		background-color: var(--bg-black);
+	}
+	.page{
+		position: absolute;
+		inset: 0;
+	}
+</style>
