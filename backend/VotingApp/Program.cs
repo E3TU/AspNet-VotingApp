83381eedb66325068173a556b9770677768377a3
Eetu <99178022+E3TU@users.noreply.github.com>
2026-02-19 20:22:17 +0200

    Added cors + services

--- backend/VotingApp/Program.cs
+++ backend/VotingApp/Program.cs
@@ -1,17 +1,31 @@
+
+using Appwrite.Services;
+using VotingApp.Appwrite;
+using VotingApp.Routes;
+using VotingApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAppwriteClient();
+builder.Services.AddSingleton<AuthService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
+app.UseCors("AllowFrontend");
+
 app.UseHttpsRedirection();
 
-
-string test = "Hello World";
-
-
-app.MapGet("/test", () =>
-{
-    return test;
-});
+app.MapAuthRoutes();
 
 app.Run();
-
